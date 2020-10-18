e73f6b22c51fec428e16fa0a66c45fd0af991eb2
SciathDev <73060038+SciathDev@users.noreply.github.com>
2020-10-19 00:03:11 +0100

    Map and Squire added
    Created a simple test map with collisions. Added a player with movement and camera follow

---
+++ DK30GJT7/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    Transform targetObject;
+
+    float smoothing = 0.1f;
+    float startingZ;
+
+    private void Awake()
+    {
+        startingZ = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector2.Lerp(transform.position, targetObject.position, smoothing);
+        transform.position = new Vector3(transform.position.x, transform.position.y, startingZ); //camera sets z to 0 after lerp
+    }
+}
