0d3c0a1746e99792db3694692986743e1d966109
SciathDev <73060038+SciathDev@users.noreply.github.com>
2020-10-20 12:02:23 +0100

    Door movement
    Player can use doors to move between rooms. Doors can be toggled open and closed and current room updated.

---
+++ DK30GJT7/Assets/Scripts/Environment/RoomManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    [SerializeField]
+    static int currentRoom = 0;
+
+    public static void UpdateCurrentRoom(int room)
+    {
+        currentRoom = room;
+        Debug.Log("Current room is now " + currentRoom);
+    }
+}
