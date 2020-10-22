a2fe11b8741bc86ed173d3f5eeffd5cb9601d693
zachary stover <zcstover@gmail.com>
2020-10-21 19:41:31 -0700

    decision tree!

---
+++ DK30GJT7/Assets/Scripts/Knight/Interest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Interest
+{
+    public float InitInterest = 50;
+    public string InterestType;
+    public float CurrentInterest;
+    public GameObject Object;
+    public bool IsActive;
+}
---
+++ DK30GJT7/Assets/Scripts/Knight/Room.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Room
+{
+    public RectInt room;
+    public float Interest;
+    public float InterestGrowth = 0.5f;
+}
