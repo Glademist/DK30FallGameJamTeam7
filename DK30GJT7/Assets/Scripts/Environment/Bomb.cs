c89d685316f26081c709c0e94efb49ca24ffe6c8
SciathDev <73060038+SciathDev@users.noreply.github.com>
2020-10-26 14:53:15 +0000

    Bombs explode after a delay

---
+++ DK30GJT7/Assets/Scripts/Environment/Bomb.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    float explodeTime = 3f;
+    float explodeRadius = 2f;
+    int damage = 3;
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject vfx = transform.GetChild(0).gameObject;
+        anim = vfx.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (explodeTime > 0)
+        {
+            explodeTime -= Time.deltaTime;
+
+            if (explodeTime < 0.3f)
+            {
+                anim.SetTrigger("Detonate");
+            }
+        }
+        else
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        Collider2D[] collisions = Physics2D.OverlapCircleAll(transform.position, explodeRadius);
+        Debug.Log(string.Format("Found {0} collisions", collisions.Length));
+        for (int i = 0; i < collisions.Length; i++)
+        {
+            Health character = collisions[i].gameObject.GetComponent<Health>();
+            if(character != null)
+            {
+                character.TakeDamage(damage);
+            }
+        }
+        Debug.Log("BOOM");
+        Destroy(gameObject);
+    }
+}
--- DK30GJT7/Assets/Scripts/Environment/SpikeTrap.cs
+++ DK30GJT7/Assets/Scripts/Environment/SpikeTrap.cs
@@ -26,6 +26,7 @@ public class SpikeTrap : MonoBehaviour
             {
                 extended = true;
             }
+
         }
         else
         {
