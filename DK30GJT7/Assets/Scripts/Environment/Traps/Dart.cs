62490f3b5d48675ad8cb01517523061bb629e504
SciathDev <73060038+SciathDev@users.noreply.github.com>
2020-11-03 22:47:56 +0000

    Tidied dart trap

--- DK30GJT7/Assets/Scripts/Environment/Traps/Dart.cs
+++ DK30GJT7/Assets/Scripts/Environment/Traps/Dart.cs
@@ -6,8 +6,15 @@ public class Dart : MonoBehaviour
 {
     public int damage = 2;
     float speed = 4f;
+
+    float timer = 0.2f;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (timer > 0)
+        {
+            return;
+        }
         Health victim = collision.gameObject.GetComponent<Health>();
         if (victim)
         {
@@ -18,6 +25,10 @@ public class Dart : MonoBehaviour
 
     private void Update()
     {
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
         transform.Translate(0, speed * Time.deltaTime, 0);
     }
 }
