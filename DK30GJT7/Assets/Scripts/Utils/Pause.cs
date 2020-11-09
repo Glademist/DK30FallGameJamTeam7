1ade79d97f1319e7b711dc2ac767d2a6b1933d57
SciathDev <73060038+SciathDev@users.noreply.github.com>
2020-11-09 18:24:19 +0000

    Pause option

---
+++ DK30GJT7/Assets/Scripts/Utils/Pause.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseScreen;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("pausing");
+            TogglePause();
+        }
+    }
+
+    void TogglePause()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            pauseScreen.SetActive(true);
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            pauseScreen.SetActive(false);
+        }
+    }
+}
