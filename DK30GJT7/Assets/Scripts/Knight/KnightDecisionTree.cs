0cb0e8598cb91977de8accfbb26ba4b3670c8e21
SciathDev <73060038+SciathDev@users.noreply.github.com>
2020-11-14 18:53:14 +0000

    .

--- DK30GJT7/Assets/Scripts/Knight/KnightDecisionTree.cs
+++ DK30GJT7/Assets/Scripts/Knight/KnightDecisionTree.cs
@@ -137,7 +137,7 @@ public class KnightDecisionTree : MonoBehaviour
         }
         if (knightMove.AccessibleRooms.Count <= 1)
         {
-            //knightMove.CheckDoors();
+            knightMove.CheckDoors();
         }
         foreach (Room Room in knightMove.AccessibleRooms)
         {
